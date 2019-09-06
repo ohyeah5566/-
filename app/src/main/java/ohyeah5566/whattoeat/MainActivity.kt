f7522913b0098677bb34dd35fa28b4806bf614c4
ohyeah5566 <ohyeah5566@gmail.com>
2019-09-06 11:24:22 +0800

    First commit

---
+++ app/src/main/java/ohyeah5566/whattoeat/MainActivity.kt
@@ -0,0 +1,12 @@
+package ohyeah5566.whattoeat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
